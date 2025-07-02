930aebddf2427255f2872c484bf5aae00d67ba2c
David Ngacha <davidngacha01@gmail.com>
2025-07-02 21:45:13 +0300

    initialized e-portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <main className="bg-blue-600 text-black font-bold text-4xl text-center justify-center h-screen p-4">
+        <p>This will be the home page for my units.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
