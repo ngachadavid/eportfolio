76bf416d164edca97851c6ea7174ae0fef26f0ef
David Ngacha <davidngacha01@gmail.com>
2025-07-03 00:34:57 +0300

    responsiveness changes

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -4,15 +4,15 @@ export default function About() {
     return (
         <main className="bg-[#132257] py-4 text-white">
            <div className='max-w-[1280px] mx-auto px-4'>
-             <div className="text-center">
+             <div className="text-start md:text-center">
                 <div className="inline-block">
-                    <h2 className="text-xl font-bold">
+                    <h2 className="text-lg md:text-xl font-bold">
                         About Me
                     </h2>
                     <div className="h-[2px] bg-white mt-2" />
                 </div>
             </div>
-            <p className="text-sm text-center max-w-2xl mx-auto mt-6 mb-10">
+            <p className="text-sm text-start md:text-center max-w-2xl mx-auto mt-6 mb-10">
                 This e-portfolio presents work completed as part of the BSc Computer Science – Data Science module. It includes coursework, reflections, and projects from units covered during the semester. <br/>
                 The goal of this portfolio is to document my learning journey, highlight practical assignments, and provide a clear record of the skills and concepts explored throughout the module.My e-portfolio based on the work carried out on the Bsc Computer Science Programme.
             </p>
--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default function Header() {
   return (
     <main className="max-w-[1280px] mx-auto pt-4 px-4 text-white">
-      <div className="text-center">
+      <div className="text-start md:text-center">
         <div className="inline-block">
-          <h1 className="text-4xl font-bold">
+          <h1 className="text-xl md:text-4xl font-bold">
             My Data Science e-Portfolio
           </h1>
           <div className="h-[2px] bg-white mt-2" />
         </div>
       </div>
 
-      <p className="text-base text-center max-w-2xl mx-auto uppercase mt-6 mb-10">
+      <p className="text-sm md:text-base text-start md:text-center max-w-2xl mx-auto uppercase mt-6 mb-10">
         My e-portfolio based on the work carried out on the BSc Computer Science Programme.
       </p>
     </main>
